24747220d48ee04299313e772b806d323e9459f9
akhil2000mittal <53169011+akhil2000mittal@users.noreply.github.com>
2020-10-17 21:31:05 +0530

    Update main.dart

--- dharma/lib/main.dart
+++ dharma/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Image(
-          image: AssetImage('images/diamond.png')
+          image: AssetImage('images/diamond.png')//comments added
           )
         ),
       )
